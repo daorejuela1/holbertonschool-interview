52e95a909fbbf4685392dd869ecb613c255aa537
daorejuela1 <daorejuela1@outlook.com>
2020-12-07 20:02:10 +0000

    Initial commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This code solve the problem of lockbox by checking the state of every box
+after traversing the keys
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Creates a register to know if the boxes are opened
+
+    Args:
+    boxes (list of list): List of boxes that contains the list of keys.
+    Returns:
+    True if all boxes can be opened, otherwise False
+    """
+    register = [0 for x in range(len(boxes))]
+    boxChecking(boxes, 0, register)
+    if all(register):
+        return True
+    return False
+
+
+def boxChecking(boxes, key, register):
+    """
+    Opens a box and asks if the boxes are already opened
+
+    Args:
+    boxes (list of list): List of boxes that contains the list of keys.
+    key: number of the box to be opened.
+    register: list of boxes opened
+
+    Returns:
+    True if all boxes can be opened, otherwise False
+    """
+    register[key] = 1
+    if all(register[x] for x in boxes[key]):
+        if all(register):
+            return True
+        return False
+    for newkey in boxes[key]:
+        boxChecking(boxes, newkey, register)
